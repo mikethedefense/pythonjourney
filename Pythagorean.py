0046ef7de8874c81c78385125360eefd60403fc0
mikethedefense <41405040+mikethedefense@users.noreply.github.com>
2018-09-30 20:07:34 -0400

    Update Pythagorean.py

--- Pythagorean.py
+++ Pythagorean.py
@@ -1,5 +1,6 @@
 import math 
-legs = int(input('What is the first leg length of the triangle?:'))
-other = int(input('Second leg length?:'))
-hypothenuse = math.sqrt(legs**2 + other**2)
-print('The hypothenuse is',hypothenuse)
+leg_1 = int(input('What is the first leg length of the triangle?:')) # leg_1 is the side of the triangle
+leg_2 = int(input('Second leg length?:')) # leg_2 is the side of the triangle
+hypothenuse = math.sqrt(leg_1**2 + leg_2**2)
+answer = round(hypothenuse,2)
+print('The hypothenuse is',answer)
