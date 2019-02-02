fb6eb15be492b7ca608efbb51495dd415b629161
mikethedefense <41405040+mikethedefense@users.noreply.github.com>
2019-02-02 17:26:08 -0500

    Update AgeValidation.py

--- AgeValidation.py
+++ AgeValidation.py
@@ -4,6 +4,6 @@ while age < 1 or age > 95:
     print('Age is not valid!')
     age = input('Please enter your age:')
     age = int(age)
-print('Thank you for entering your age!')
+print('Thank you for validating your age!')
 
 
