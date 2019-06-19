f3300c729a41f941e524f88cfa2d5f051bcd1d85
mikethedefense <41405040+mikethedefense@users.noreply.github.com>
2019-06-18 21:44:21 -0400

    Create circlev0.1.py

---
+++ circlev0.1.py
@@ -0,0 +1,10 @@
+def circle(radius):
+    pi=3.14
+    area = pi*radius**2
+    diametre=radius*2
+    circumference = pi*diametre
+    print("Area: {} \nCircumference: {} ".format(area,circumference))
+
+if __name__=='__main__':
+    circle(8) #Try different Radiis
+
