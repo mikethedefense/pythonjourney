53b439f1bcf94679d5d593323a7976b0e32a32c7
mikethedefense <41405040+mikethedefense@users.noreply.github.com>
2020-09-22 20:47:09 -0400

    Final commit
    Added command line commands using argparse. Removed some useless code.

--- convertv0.01.py
+++ convertv0.01.py
@@ -1,50 +1,52 @@
 #  Script that converts a file from one encoding to another. 
 
-# -------To do---------
-# 1. Command line should accept an input filename
-# 2. Input Encoding (default to UTF-8)
-# 3. Output Encoding
-# 4. Handle Errors
-#----------------------
-
 #!usr/bin/env python3 
 
 import sys
 import logging
+import argparse
 
-__version__ = '0.01'
+__version__ = '1.00'
 
 logging.basicConfig(level = logging.DEBUG) 
 
 class Encode:
+    """
+    Class that converts a file from one encoding to another
+    """
    
-    def __init__(self, fin, output_encode, input_encode):
+    def __init__(self, fin, input_encode, output_encode):
         self.input_encode = input_encode
         self.output_encode = output_encode
         self.fin = fin
     
     def run(self):
-        lis = []
+        """
+        Logic for encode conversion
+        """
         logging.debug(f"Converting file from {self.input_encode}")
-        with open(self.fin, encoding = self.input_encode) as fout:
-            for line in fout:
-                lis.append(line)
-        with open(self.fin, 'w', encoding = self.output_encode) as fout:
-            for word in lis:
-                fout.write(word)
+        with open(self.fin, encoding = self.input_encode) as file_1:
+            content = file_1.read()
+        with open(self.fin, 'w', encoding = self.output_encode) as file_2:
+            file_2.write(content)
         logging.debug(f"File converted to {self.output_encode}")
         
-def main():
-   encode = Encode(sys.argv[1], sys.argv[2], sys.argv[3])
-   encode.run()
-
-def log_error(error):
-    return error
-
+def main(args):
+    """
+    Logic to run Arguments
+    """
+    parser = argparse.ArgumentParser(description= "Convert file from one encoding to another")
+    parser.add_argument("-f","--file", type = str, required = True, help = "File input")
+    parser.add_argument("-i","--input", type = str, default = "utf-8", help = "The encoding you want to input")
+    parser.add_argument("-o","--output", type = str, required = True, help = "The encoding you want as your output")
+    parser.add_argument("-v", "--version", action = 'version', version = __version__)
+    args = parser.parse_args()
+    encode = Encode(args.file, args.input, args.output)
+    encode.run()
+    logging.debug("Done")
+    
 if __name__ == '__main__':
     try:
-        main()
-    except (FileNotFoundError, ValueError, IndexError, LookupError) as e:
-        logging.debug(log_error(e))
-        
-        # Add argparse commands next 
+        main(sys.argv[1:])
+    except (FileNotFoundError, LookupError) as e:
+        logging.debug(e)
