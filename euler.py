d10c680f1bfe7ff03276d70cefba7dc3c95120ce
FARMER <41405040+mikethedefense@users.noreply.github.com>
2024-09-26 09:45:04 -0400

    Update euler.py

--- euler.py
+++ euler.py
@@ -1,40 +1,40 @@
 # Estimation using Euler's method
 from sympy import *
-x,y = symbols('x y') # The variables here are arbitrary. 
 
-def euler(func, step_size,*values):
+x,y = symbols('x y')
+
+def euler(func, x_initial, y_initial, step_size,*values):
     """This returns the euler approximation of simple 2D functions. 
     To use:, input the function as the first argument. 
-        - Example of a correct function: 2*x + 3*y
-        - Example of an incorrect function: 2x + 3y. 
-    In the second argument input the step_size, commonly known as the 'h' value
-    And in the remaining arguments, input all the values you want to approximate (can be 1,2,3 to n values). 
+        - Example of a correct functions: 
+            2*x + 3*y
+            x**4 + 3*x*y + 3*x
+        - Example of incorrect functions: 
+            3x + 2y
+            3*t + 2*z (The function must only be in x and y)
+    The second and third argument take in the initial x value and y value.
+    In the third argument, input the step_size, commonly known as the 'h' value
+    And in the remaining arguments, input all the values you want to approximate. 
     Example of use: 
-    >>> euler(3- 2*x - 0.5*x, 0.1, 1,2,3,4,5)
+    >>> euler(3- 2*x - 0.5*y,0,1, 0.1, 1,2,3,4,5)
     2.216419789901074
     1.3396830086889504
     -0.7903039312581901
     -3.6706580353463476
     -7.000284678597275
     
-    IMPORTANT: Make sure to 
-    >>> import sympy 
-    
-    AND to define your symbols using
-    >>> a,b = symbols('a b')
+    You can import this function like so:
+    >>> from euler import *
     """
     f = lambdify([x,y],func)
     for i in values: # Loop through the values list.
 
         # Initial values 
-        t_point = 0 
-        y_point = 1 
+        t_point = x_initial 
+        y_point = y_initial
 
-        value_range = int(i / step_size)
-        for i in range(value_range):
+        value_range = round(i / step_size)
+        for k in range(value_range):
             y_point = y_point + f(t_point,y_point) * step_size
             t_point += step_size
         print(y_point)
-
-# Call the function here:
-euler(3 - 2*x - 0.5*y,0.05,1,2,3,4,5) 
